c9ee1f4e8ad900da04a104b24a153d8d27d94650
BlobbyBob <swierzy@uni-bonn.de>
2020-03-12 17:25:09 +0100

    Added Audio Switch + Readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Friendly Scripts
+
+Just some nice scripts that might help you in your everyday life.
+
+## Contents
+
+#### AudioSwitch
+
+The AudioSwitch can be assigned to a shortcut and cycles through the speakers of your choice.
+
+
---
+++ audio-switch.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+# Devices to switch between
+declare -a devNames=("alsa_output.pci-0000_0d_00.4.analog-stereo"
+		"alsa_output.usb-Logitech_G533_Gaming_Headset-00.analog-stereo"
+    "alsa_output.pci-0000_0d_00.4.analog-stereo")
+declare -a portNames=("analog-output-lineout" "analog-output" "analog-output-headphones")
+declare -a displayNames=("Speakers" "Wireless Headset" "Wired Headset")
+declare -a defaultVolumes=(65535 15000 15000)
+
+# If the output length of the pacmd list-sinks command changes this needs to be adjusted
+sinkInfoSize=70
+
+# Get current device
+runningName=$(pacmd list-sinks | grep -B 3 "state: RUNNING" | grep "name:" | grep -oE "<.+>" | grep -oE "[^<>]+")
+runningPort=$(pacmd list-sinks | grep -A $sinkInfoSize RUNNING | tail -20 | grep "active port" | grep -oE "<.+>" | grep -oE "[^<>]+")
+index=
+for (( i=0; i<${#devNames[@]}; i++ ));
+do
+	if [[ "${devNames[$i]}" == "$runningName" ]] && [[ "${portNames[$i]}" == "$runningPort" ]]; then
+		index=$i
+		break
+	fi
+done
+
+# Get current input programs indices
+programs=$(pacmd list-sink-inputs| grep "index:" | grep -oE "[0-9]+")
+
+# Get next available device+port
+new=$index
+while : ; do
+	new=$(( ($new+1) % ${#devNames[@]} ))
+	if [[ $new == $index ]]; then
+		break # No other device available
+	fi
+	available=$(pacmd list-sinks | grep -A $sinkInfoSize "${devNames[$new]}" | grep -A 8 "ports:" | grep "${portNames[$new]}" | grep -oE "available: [a-z]+" | grep -oE ": [a-z]+" | grep -oE "[a-z]+")
+	if [[ "$available" == "yes" ]] || [[ "$available" == "unknown" ]]; then
+
+		# Move inputs if device changes
+		if [[ "${devNames[$index]}" != "${devNames[$new]}" ]]; then
+			while read -r line; do
+				pacmd move-sink-input "$line" "${devNames[$new]}"
+			done <<< "$programs"
+		fi
+
+		pacmd set-sink-volume "${devNames[$new]}" ${defaultVolumes[$new]}
+		pacmd set-sink-port "${devNames[$new]}" "$portNames[$new]"
+		pacmd set-default-sink "${devNames[$new]}"
+		notify-send 'Audio Switch' "Current device: ${displayNames[$new]}" --icon=audio-volume-high
+		break
+	fi
+done
