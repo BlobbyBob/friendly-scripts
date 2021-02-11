33f6517ffa331fa134c0d6e0c65d0062ae006837
BlobbyBob <swierzy@uni-bonn.de>
2021-02-11 10:29:21 +0100

    Backup script now properly exits on error

--- backup.sh
+++ backup.sh
@@ -2,96 +2,94 @@
 
 mnt="/mnt/bck"
 incrementalDisk="/dev/disk/by-uuid/dbb5e1e2-64d7-4a53-a04f-18671dfd1c01"
-incrementalPaths=("/etc" "/home" "/root" "/source" "/srv" "/var")
+incrementalPaths="/root /source /srv /etc /var /home"
 fullDisks=("/dev/disk/by-uuid/8dcebdc8-e6cf-49ba-985c-fbbd622425d1" "/dev/disk/by-uuid/045c7eaf-10cf-425a-a22d-cb1c9b771b23")
-fullPaths=("/aur" "/bin" "/boot" "/efi" "/etc" "/home" "/lib" "/lib64" "/lost+found" "/opt" "/root" "/sbin" "/source" "/srv" "/sys" "/usr" "/var")
-export log=$(mktemp)
+fullPaths="/aur /bin /boot /efi /etc /home /lib /lib64 /lost+found /opt /root /sbin /source /srv /usr /var"
+export log=$(mktemp -p '/root/log')
 date > "$log"
 
 check () {
     if [[ $? -ne 0 ]]; then
-        printf "\e[31mFAIL\e[0m\n"
-        [[ -n "$1" ]] && echo "$1" >&2
-        printf "\n"
+        printf "\e[31mFAIL\e[0m\n" | tee -a "$log" >&2
+        [[ -n "$1" ]] && echo "$1" | tee -a "$log" >&2
+        printf "\n" | tee -a "$log"
         echo "Have a look at the logfile at: $log" >&2
         exit 1
     else
-        printf "\e[32mOK\e[0m\n"
+        printf "\e[32mOK\e[0m\n" | tee -a "$log"
     fi
 }
 
-printf "[i] Running as user $(whoami)\n"
-printf "[*] Checking privileges ... "
+printf "[i] Running as user $(whoami)\n" | tee -a "$log"
+printf "[*] Checking privileges ... " | tee -a "$log"
 [[ $(id -u) -eq 0 ]]
 check "[i] Script must be run as root."
 
-printf "[*] Checking mountpoint ... "
+printf "[*] Checking mountpoint ... " | tee -a "$log"
 mountpoint -q "$mnt" 2>>"$log" >>"$log"
 [[ $? -ne 0 ]]
 check "[i] Mountpoint $mnt already mounted"
 
 if [[ "$1" == "--incremental" ]] || [[ "$1" == "-i" ]]; then
 
-    printf "[*] Performing incremental backup\n"
-    printf " |- Checking for backup disk ... "
+    printf "[*] Performing incremental backup\n" | tee -a "$log"
+    printf " |- Checking for backup disk ... " | tee -a "$log"
     ls -lah "$incrementalDisk" 2>>"$log" >>"$log"
     check "[!] Can't find backup disk"
 
-    printf " |- Mounting disk ... "
+    printf " |- Mounting disk ... " | tee -a "$log"
     mount "$incrementalDisk" "$mnt" 2>>"$log" >>"$log"
     check
 
     export BUP_DIR="$mnt"
 
-    printf " |- Creating index ... "
+    printf " |- Creating index ... " | tee -a "$log"
     bup index --exclude-rx="/\\.?(C|c)ache/" $incrementalPaths 2>>"$log" >>"$log"
     check
 
-    printf " |- Backing up ... "
-    bup save -n local $incrementalPaths 2>>"$log" >>"$log"
+    printf " |- Backing up ... " | tee -a "$log"
+    bup save -vv -n local $incrementalPaths 2>>"$log" >>"$log"
     check
 
-    printf " |- Unmounting ... "
+    printf " |- Unmounting ... " | tee -a "$log"
     umount "$incrementalDisk" 2>>"$log" >>"$log"
     check
 
-    printf " |- Syncing ... "
+    printf " |- Syncing ... " | tee -a "$log"
     sync 2>>"$log" >>"$log"
     check
 
-    printf "[i] Done\n\n"
-    rm "$log"
+    printf "[i] Done\n\n" | tee -a "$log"
 
 
 elif [[ "$1" == "--full" ]] || [[ "$1" == "-f" ]]; then
 
-    printf "[*] Performing full backup\n"
-    printf " |- Checking disk selection ... "
+    printf "[*] Performing full backup\n" | tee -a "$log"
+    printf " |- Checking disk selection ... " | tee -a "$log"
     [[ ! -z "$2" ]] && [[ ! -z "${fullDisks[$2]}" ]]
     check "[!] Disk selection '$2' (Disk '${fullDisks[$2]}') invalid\n"
 
-    printf " |- Checking for backup disk ... "
+    printf " |- Checking for backup disk ... " | tee -a "$log"
     ls -lah "${fullDisks[$2]}" 2>>"$log" >>"$log"
     check "[!] Can't find backup disk"
 
-    printf " |- Mounting disk ... "
+    printf " |- Mounting disk ... " | tee -a "$log"
     mount "${fullDisks[$2]}" "$mnt" 2>>"$log" >>"$log"
     check
 
-    printf " |- Backing up (this may take some time) ... "
-    rsync -a --delete --quiet -HAXE $fullPaths "$mnt" 2>>"$log" >>"$log"
+    printf " |- Backing up (this may take some time) ... " | tee -a "$log"
+    rsync -rlptgoDHAXE --progress --delete $fullPaths "$mnt" 2>>"$log" >>"$log"
     check
 
-    printf " |- Unmounting ... "
+    printf " |- Unmounting ... " | tee -a "$log"
     umount "${fullDisks[$2]}" 2>>"$log" >>"$log"
     check
 
-    printf " |- Syncing ... "
+    printf " |- Syncing ... " | tee -a "$log"
     sync 2>>"$log" >>"$log"
     check
 
-    printf "[i] Done\n\n"
-    rm "$log"
+    printf "[i] Done\n\n" | tee -a "$log"
 
 else
 
@@ -99,6 +97,7 @@ else
     printf "[i] Valid options:\n"
     printf " |- -f, --full           Perform a full system backup\n"
     printf " |- -i, --incremental    Perform an incremental and partial backup\n"
+    rm "$log"
     exit 1
 
 fi
